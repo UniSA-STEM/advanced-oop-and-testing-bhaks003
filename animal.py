8066dab741dbeafa94004bef709dea3909d9380e
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-09 21:09:45 +1030

    initial commit changed name, desc and other things

--- animal.py
+++ animal.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
+File: animal.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Krish Bhadani
+ID: 110429045
+Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
