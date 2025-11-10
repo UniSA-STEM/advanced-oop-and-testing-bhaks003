bd0d73db0aec67c60542fd232dc3ca2013cc175e
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-10 18:17:22 +1030

    created a child bird class from animal class and gave attributtes

---
+++ bird.py
@@ -0,0 +1,16 @@
+'''
+File: bird.py
+Description: A brief description of this Python module.
+Author: Krish Bhadani
+ID: 110429045
+Username: bhaks003
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from animal import Animal
+
+class Bird(Animal):
+    def __init__(self, species, name, age, diet):
+        super().__init__(species, name, age, diet, "bird")
+
+
