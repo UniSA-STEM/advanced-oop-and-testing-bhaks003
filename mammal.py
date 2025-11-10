c43a542ff89bdf8ee53c3c8ddc457a604157d0b0
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-10 18:14:53 +1030

    adding make_sound and daily_care method which will the display the message appropriately

--- mammal.py
+++ mammal.py
@@ -12,3 +12,9 @@ from animal import Animal
 class Mammal(Animal):
     def __init__(self, species, name, age, diet):
         super().__init__(species, name, age, diet, 'mammal')
+
+    def make_sound(self):
+        return self.get_name() + "the" + self.get_species() + "is making a deep mammal sound"
+
+    def daily_care(self):
+        return self.get_name() + "is required a land space which is warmth and has regular feeding"
