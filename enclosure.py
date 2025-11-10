1a6996f61fcffddff4df1ae73874d59a74c2cd01
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-10 22:32:19 +1030

    created a enclosure class and defined the attributes

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,14 @@ Author: Krish Bhadani
 ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclosure:
+    def __init__(self, name, environement_type, size_sqkm, animal_type):
+        self.__name = name
+        self.__environement_type = environement_type
+        self.__size_sqkm = size_sqkm
+        self.__animal_type = animal_type
+        self.__cleanliness_level = "Clean"
+        self.__animals = []
+
