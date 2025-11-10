1b3f42d5d327bd757be19c1b947ea25c5bb41770
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-10 17:44:31 +1030

    instantiated the Animal class and gave the appropriate attributes as well

--- animal.py
+++ animal.py
@@ -5,4 +5,19 @@ Author: Krish Bhadani
 ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, species, name, age, diet, category):
+        self.__species = species
+        self.__name = name
+        self.__age = age
+        self.__diet = diet
+        self.__category = category
+        self.__enclosure = []
+        self.__health_records = []
+
+
