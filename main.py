f56703c679e59d7530c5010df05ee442e57fb9ca
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-11 20:45:11 +1030

    calling all classes in main.py and testing the output

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ Author: Krish Bhadani
 ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+from mammal import Mammal
+from bird import Bird
+from reptile import Reptile
+from health_record import HealthRecord
+from staff import Staff
+from zookeeper import Zookeeper
+from veterinarian import Veterinarian
+from  enclosure import Enclosure
+
+
+print(f"===== Creating Animals ====")
+lion = Mammal("Lion", "Simba", 21, "Meat")
+crow = Bird("Crow", "Kallu", 2, "Seed")
+snake = Reptile("Snake", "Nagin", 6, "Rat")
+
