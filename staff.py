0b0bdaabf7bf64747380c729708f9270cc167de7
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-11 20:17:31 +1030

    created a staff class with attributes and added getters to return the value

--- staff.py
+++ staff.py
@@ -5,4 +5,17 @@ Author: Krish Bhadani
 ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+from health_record import HealthRecord
+
+class Staff:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def get_name(self):
+        return self.name
+
+    def get_role(self):
+        return self.role
