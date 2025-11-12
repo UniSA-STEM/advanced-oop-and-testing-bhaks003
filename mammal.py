b39ba1b7802f31ac92687401df528887172a085e
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-12 12:04:02 +1030

    added comment in mammal.py and reptile.py classes and tested the output

--- mammal.py
+++ mammal.py
@@ -10,11 +10,25 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from animal import Animal
 
 class Mammal(Animal):
+    # this mammal class inherits from an abstract Animal base class can call parent class as well
+    # this class will represent all the mammal in the zoo and also define their specific traits
     def __init__(self, species, name, age, diet):
         super().__init__(species, name, age, diet, 'mammal')
 
     def make_sound(self):
-        return self.get_name() + "the" + self.get_species() + "is making a deep mammal sound"
+        """
+                overiding the method passed from the parent class and changing according to child class need
+                in this scenario it will the display the name species and sound bird's make
+        """
+        return self.get_name() + " the " + self.get_species() + " s making a deep mammal sound"
 
+    """
+        orviding of the abstract daily_care method 
+        returns the string stating about the daily care needed
+    """
     def daily_care(self):
-        return self.get_name() + "is required a land space which is warmth and has regular feeding"
+        return self.get_name() + " is required a land space which is warmth and has regular feeding"
+
+# lion = Mammal("Lion", "Simba", 5, "Meat")
+# print(lion.make_sound())
+# print(lion.daily_care())
--- reptile.py
+++ reptile.py
@@ -10,11 +10,25 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from animal import Animal
 
 class Reptile(Animal):
-    def __init__(self, species, name, age, diet):
+    # this reptile class inherits from an abstract Animal base class can call parent class as well
+    def __init__(self, species, name, age, diet):  # this class will represent all the reptile in the zoo and also define their specific traits
         super().__init__(species, name, age ,diet, "reptile")
 
     def make_sound(self):
-        return self.get_name() + "the" + self.get_species() + "makes hisss or growls quietly"
+        """
+                overiding the method passed from the parent class and changing according to child class need
+                in this scenario it will the display the name species and sound bird's make
+        """
+        return self.get_name() + " the " + self.get_species() + " makes hisss or growls quietly"
 
+    """
+        orviding of the abstract daily_care method 
+        returns the string stating about the daily care needed
+    """
     def daily_care(self):
-        return self.get_name() + "requries a dry arera, warm and temperature control system as well."
+        return self.get_name() + " requries a dry arera, warm and temperature control system as well."
+
+
+# snake = Reptile("Python", "Sly", 4, "Rats")
+# print(snake.make_sound())
+# print(snake.daily_care())
