c5c64f48239a2fc76de8282e729fd104afd7cf18
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-12 16:13:18 +1030

    done with commenting for staff and testing the outputs as well

--- staff.py
+++ staff.py
@@ -7,9 +7,11 @@ Username: bhaks003
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
-from health_record import HealthRecord
-
 class Staff:
+    """
+    creating class staff which has attributes like name and role
+    and with that using exception to raise error if invalid input given
+    """
     def __init__(self, name, role):
         if not name:
             raise ValueError("Name cannot be empty")
@@ -18,11 +20,20 @@ class Staff:
         self.__name = name
         self.__role = role
 
+
+    #using getters
     def get_name(self):
         return self.__name
 
     def get_role(self):
-        return self__role
+        return self.__role
 
+    # string conversion method
     def __str__(self):
         return f"Staff: {self.__name} | Role: {self.__role}"
+
+
+# staff_member = Staff("Alex", "Zookeeper")
+# print(staff_member.get_name())
+# print(staff_member.get_role())
+# print(staff_member)
