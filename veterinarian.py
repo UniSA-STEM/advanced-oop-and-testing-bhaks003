c6f441669f829f4c4c17fce7fb6a03d45b23d20e
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-12 16:50:56 +1030

    commenting for the zookeeper and veterinarian

--- veterinarian.py
+++ veterinarian.py
@@ -10,10 +10,18 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from health_record import HealthRecord
 from staff import Staff
 
+"""
+the veterinarian class inherits from the base staff class
+this is a for vet who performs health checks on animal
+"""
 class Veterinarian(Staff):
     def __init__(self, name):
         super().__init__(name, "Veterinarian")
 
     def conduct_checkup(self, animal, description, severity, treatment, date_reported):
+        """
+        method will perform health checks on animal and create a health record if none found 
+        it will store the description, severity, treatment and date_reported for each animal 
+        """
         recordings = HealthRecord(description, severity, treatment, date_reported)
         return f"{self.get_name()} Recorded: {recordings}"
--- zookeeper.py
+++ zookeeper.py
@@ -9,13 +9,22 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 from staff import Staff
 
+
 class Zookeeper(Staff):
+    """
+    in the zookeeper class which inherit from staff class
+    it represent a zoo staff who takes care of feeding and cleaning the enclosure
+    for daily purpose
+    """
     def __init__(self, name):
         super().__init__(name, "Zookeeper")
 
+    # this function is return the string statement about zookeeper feeding the animal
     def feed_animal(self, name):
         return f"{self.get_name()} is feeding animal {animal.get_name()} which {animal.get_species()}"
 
+    """
+    this method will return the string statement of cleaning the enclosure will make the enclosure liveable again
+    """
     def clean_enclosure(self, enclosure):
         return f"{self.get_name()} is cleaning enclosure {enclosure.clean()}"
-
