d00a235e5f85edb843873e24543755e45ff8cbc4
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-12 17:17:06 +1030

    till now testing the animal and its subclass and also enclosure as well

--- main.py
+++ main.py
@@ -22,3 +22,38 @@ lion = Mammal("Lion", "Simba", 21, "Meat")
 crow = Bird("Crow", "Kallu", 2, "Seed")
 snake = Reptile("Snake", "Nagin", 6, "Rat")
 
+print("\n")
+
+print("====== Showing the string conversion implementation. =============")
+print(lion)
+print(crow)
+print(snake)
+
+print("\n")
+
+print("====== Testing the method of make_sound. ========")
+print(lion.make_sound())
+print(crow.make_sound())
+print(snake.make_sound())
+
+print("\n")
+
+print("====== Testing the method of daily_care. ======")
+print(lion.daily_care())
+print(crow.daily_care())
+print(snake.daily_care())
+
+print("\n")
+
+print("======== Setting Up the enclosures and including animals. ========== ")
+woodland = Enclosure("Black Woodland", "Woodland", 3, "mammal")
+aviary = Enclosure("Aviary", "Aviary", 4, "bird")
+lagoons = Enclosure("Lagoon", "Lagoon", 5, "reptile")
+
+
+print("\n")
+
+print("========== showing the string conversion implementation. =======")
+print(woodland)
+print(aviary)
+print(lagoons)
