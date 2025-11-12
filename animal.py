6252c757680ab6c9059243b5da95ab85f109f0bb
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-12 11:50:58 +1030

    adding the comment and explaing the code structure

--- animal.py
+++ animal.py
@@ -12,7 +12,17 @@ from abc import ABC, abstractmethod
 from health_record import HealthRecord
 
 class Animal(ABC):
+    """
+    abstract class representing an generic animal present in the zoo.
+    all animals share will share core attributtes such as species, name, age, diet and category
+    subclasses will implement make_sound() and daily_care() to define their unique behaviour
+    """
     def __init__(self, species, name, age, diet, category):
+        """
+        Instantiate an animal with species, name, age, diet, and category attributes.
+        creating an enclosure list
+        and health record list
+        """
         self.__species = species
         self.__name = name
         self.__age = age
@@ -21,6 +31,9 @@ class Animal(ABC):
         self.__enclosure = []
         self.__health_records = []
 
+    # ----------------
+    # using getters methods return info
+    # -------------------
     def get_category(self):
         return self.__category
 
@@ -31,31 +44,50 @@ class Animal(ABC):
         return self.__name
 
     def feed(self):
-        return self.__name + "the" + self.__species + "is eating" + self.__diet + "."
+        """
+        showcase about animal eating food
+        return with a string which shows which animal is eating food
+        """
+        return self.__name + "the " + self.__species + " is eating " + self.__diet + "."
 
+
+    """
+    in this sleep method it will return the animal name which sleeping 
+    """
     def sleep(self):
-        return self.__name + "the" + self.__species + "is sleeping calmly."
+        return self.__name + "the " + self.__species + " is sleeping calmly."
 
     def assign_enclosure(self, enclosure):
+        # assigning an enclosure to an animal according to category
         self.__enclosure = enclosure
 
     @abstractmethod
     def make_sound(self):
+        # this abstract methodn will be inherited in the child class
         pass
 
     @abstractmethod
-    def daily_care(self):
+    def daily_care(self): #abstract method which will be implemented in child classes
         pass
 
+    """
+    Health Management methods
+    """
     def add_health_issues(self, description, severity, treatment):
-        record = HealthRecord(description, severity, treatment)
+        # in this method animal's health record can be added which will save the desciption, severity, treatment given and date it was reported
+        record = HealthRecord(description, severity, treatment, date_reported)
         self.__health_records.append(record)
 
+    # this method will resolve the health recrod as resolved and will include the notes for future diagnose
     def resolve_health_issue(self, index, notes):
         if index < 0 or index >= len(self.__health_records):
             raise IndexError("Invalid health record index")
-        self.__health_records[index].resolve(notes)
+        self.__health_records[index].mark_resolved(notes)
 
+    """
+    has_serious_active_issues will tell us about the animal health severity which are high or serious
+    it will return true if has an active health issue and false otherwise
+    """
     def has_serious_active_issues(self):
         i = 0
         while i < len(self.__health_records):
@@ -67,9 +99,13 @@ class Animal(ABC):
 
     def get_health_summary(self):
         summary = []
+        #this method returns a list of summaries for all record associated with the animal
         for record in self.__health_records:
             summary.append(str(record))
         return summary
 
+    #++++++++++++++++++++
+    # String conversion method will display Name (Species, Category, age)
+    #+++++++++++++++++++
     def __str__(self):
         return self.__name + " (" + self.__species + ", " + self.__category + ", age " + str(self.__age) + ")"
