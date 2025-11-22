1c2b1e7550b314405e9d606d368da94daba90ec6
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-11-22 17:30:06 +1030

    adding comments

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from zookeeper import Zookeeper
 from veterinarian import Veterinarian
 from enclosure import Enclosure
 
+
+"in this section we will create the animals based on the category"
+
 print("===== Creating Animals =====")
 lion = Mammal("Lion", "Simba", 21, "Meat")
 crow = Bird("Crow", "Kallu", 2, "Seed")
@@ -25,26 +28,33 @@ print(lion)
 print(crow)
 print(snake)
 
+# testing the output of the animals making the sound
 print("\n=== Testing make_sound() method ===")
 print(lion.make_sound())
 print(crow.make_sound())
 print(snake.make_sound())
 
+# testing daily care method
 print("\n=== Testing daily_care() method ===")
 print(lion.daily_care())
 print(crow.daily_care())
 print(snake.daily_care())
 
+"""
+assigning the encloure according the category of defined animals
+"""
 print("\n=== Setting Up Enclosures and Adding Animals ===")
 woodland = Enclosure("Black Woodland", "Woodland", 3, "mammal")
 aviary = Enclosure("Aviary", "Aviary", 4, "bird")
 lagoons = Enclosure("Lagoon", "Lagoon", 5, "reptile")
 
-woodland.add_animal(lion)
+
+woodland.add_animal(lion) #checking method works, by adding and removing the animals as well
 aviary.add_animal(crow)
 lagoons.add_animal(snake)
 woodland.remove_animal("Simba")
 
+# printing output of each enclosures to display animals in it
 print("\n=== Checking Animals in Each Enclosure ===")
 print(woodland)
 print(woodland.list_animals())
@@ -53,23 +63,31 @@ print(aviary.list_animals())
 print(lagoons)
 print(lagoons.list_animals())
 
+
+"""
+instantiating the staff member by the task 
+"""
 print("\n=== Creating Staff ===")
 zookeeper = Zookeeper("Chachu")
 veterinarian = Veterinarian("Dr. Chichu")
 print(zookeeper)
 print(veterinarian)
 
+# in here each staff member will perfomr task
 print("\n=== Staff Performing Tasks ===")
 print(zookeeper.clean_enclosure(woodland))
 print(zookeeper.feed_animal(lion))
 print(veterinarian.conduct_checkup(lion, "Gastro infection", "serious", "Operate and rest", "13/11/25"))
 
+"recording the health record and updating that as well"
 print("\n=== Health Record Demonstration ===")
 record = HealthRecord("Wing injury", "high", "Bandage and rest", "10/11/25")
 print(record)
 record.mark_resolved("Healed completely after 5 days.")
 print(record)
 
+
+#tracking on particular animal health record
 print("\n=== Demonstrating Health Tracking for Animal Objects ===")
 lion.add_health_issues("Fever", "moderate", "Antibiotics and rest", "13/11/25")
 lion.add_health_issues("Fractured paw", "high", "Bandage and restricted movement", "14/11/25")
